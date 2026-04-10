73c0a5a5e2dc5940b1edfe813844733b13dc9f5e
zmh964767 <zmh964767@gmail.com>
2026-04-10 17:56:50 +0800

    fix: 配置静态导出

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  experimental: {
-    serverComponentsExternalPackages: ["pdfkit"],
+  output: "export",
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
   },
 };
 
